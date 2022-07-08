9aff10461e1a13bf8a22da743a984bca64d57010
HP <aiza0000123@gmail.com>
2022-07-08 16:29:14 +0600

    while and scanner

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,17 @@
 public class Driver {
-    private String id;
+    private int id;
     private String name;
 
-    public Driver(String id, String name) {
+    public Driver(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
